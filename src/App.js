1c76fe19c459d6b2022beceb1d2a470c0c0fa402
Alfredo Torres <amtrosul@hotmail.com>
2022-03-02 17:15:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navibar from './components/layout/Navibar';
+import Footer from './components/layout/Footer';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+
+import {Row, Container, Col} from 'react-bootstrap'
+
+import Project from './components/pages/Project';
+
+
+
 
 function App() {
+
+
+  const appStyles = {marginTop: "1.5em", paddingTop: "2em", paddingBottom: "4em"};
+  const padding = {padding: "2em"}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navibar/>
+        <Col lg={12} style={appStyles}>
+          <div style={padding}>
+
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/company" element={<Company/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/newproject" element={<NewProject/>}/>
+                <Route path="/projects" element={<Projects/>}/>
+                <Route path="/project/:id" element={<Project/>}/>
+            </Routes>
+          </div>
+        </Col>
+      <Footer/>
+
+    </Router>
   );
 }
 
 export default App;
+
+
---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,44 @@
+import {useNavigate} from 'react-router-dom'
+
+import styles from './NewProject.module.css'
+import ProjectForm from '../project/ProjectForm'
+
+function NewProject(){
+
+    const navigate = useNavigate();
+
+    function createPost(project){
+        // initialize cost and services
+        project.cost = 0;
+        project.services = [];
+
+        fetch("http://localhost:5000/projects", {
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(project)
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            // console.log(data);
+            //redirect
+            navigate("/projects", {state: {title: "Novo projeto", message: "Projeto criado com sucesso."}});
+        })
+        .catch(err => console.log(err))
+
+    }
+
+
+
+    return(
+       <div className={styles.newProjectContainer}>
+           <h1>Criar Projeto</h1>
+           <p>Crie seu projeto para depois adicionar os serviços.</p>
+
+           <ProjectForm handleSubmit={createPost} btnText="Criar projeto"/>
+       </div>
+    )
+}
+
+export default NewProject
